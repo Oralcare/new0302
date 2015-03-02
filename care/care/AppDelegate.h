b72fcd97970fd949924c887ceeff1039bf73ceae
zerozheng <641486122@qq.com>
2015-03-03 00:21:20 +0800

    初步建立项目，但是启动图片显示不出来

---
+++ care/care/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  care
+//
+//  Created by zerozheng on 15/3/2.
+//  Copyright (c) 2015年 oralcare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ care/care/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  care
+//
+//  Created by zerozheng on 15/3/2.
+//  Copyright (c) 2015年 oralcare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
